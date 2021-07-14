6d7e8222a63585e93aaae3b2548da97555b39be6
hungto228@gmail.com <hungto228@gmail.com>
2021-07-14 17:15:50 +0700

    EdgeInsests distance

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,29 @@ import 'package:flutter/material.dart';
 
 // the main function start point for all app
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('i am rhingich'),
-        backgroundColor: Colors.blueGrey,
-      ),
-      body: Center(
-        child: Image(
-          image: AssetImage('images/haha.jpg'),
-        ),
-      ),
-    ),
-  ));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('i am rhingich'),
+            backgroundColor: Colors.blueGrey,
+          ),
+          body: SafeArea(
+            child: Container(
+              margin: EdgeInsets.zero,
+              height: 100,
+              width: 100,
+              color: Colors.red,
+              child: Text("chaof"),
+            ),
+          )),
+    );
+  }
 }
