6af87fffd290019ddaddfcec46b93d509a1b1e3d
hungto228@gmail.com <hungto228@gmail.com>
2021-07-14 15:51:59 +0700

    scaffold netWorkImage

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,20 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+// the main function start point for all app
 void main() {
   runApp(MaterialApp(
-    home: SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.pink,
-          title: Text('cua nang flutter'),
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('i am rhingich'),
+        backgroundColor: Colors.blueGrey,
+      ),
+      body: Center(
+        child: Image(
+          image: NetworkImage(
+              "https://scontent-hkg4-2.xx.fbcdn.net/v/t1.6435-9/217410506_3599727820129199_5025664587525944081_n.jpg?_nc_cat=110&ccb=1-3&_nc_sid=825194&_nc_ohc=dsU9ljxdT2QAX993j8X&tn=mL_qVgmRIBnWdVmx&_nc_ht=scontent-hkg4-2.xx&oh=51d94b6a4c9af3c678ebf31a0739e217&oe=60F2B14C"),
         ),
-        body: NewWidget(),
       ),
     ),
   ));
-}
-
-class couter extends StatelessWidget {
-  late int countter;
-
-  @override
-  Widget build(BuildContext context) {
-    return Text("tui laf widget data hien tai$countter");
-  }
-
-
-}
-
-class NewWidget extends StatelessWidget {
-  const NewWidget({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text("chao cau"),
-        Row(
-          children: [
-            FlatButton(
-              onPressed: () {},
-              child: Text("red"),
-              color: Colors.red,
-            ),
-            FlatButton(
-              onPressed: () {},
-              child: Text("red"),
-              color: Colors.green,
-            ),
-            FlatButton(
-              onPressed: () {},
-              child: Text("yellow"),
-              color: Colors.yellow,
-            )
-          ],
-        )
-      ],
-    );
-  }
 }
