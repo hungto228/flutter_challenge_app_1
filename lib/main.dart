4500c003009c18780b9f82b9d944e7ba00a8f971
hungto228@gmail.com <hungto228@gmail.com>
2021-07-15 14:42:33 +0700

    flat button click

--- lib/main.dart
+++ lib/main.dart
@@ -3,85 +3,42 @@ import 'package:flutter/material.dart';
 
 // the main function start point for all app
 void main() {
-  runApp(MyApp());
+  return runApp(MaterialApp(
+    home: Center(
+      child: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text("dicee"),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
+class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-            backgroundColor: Colors.teal,
-            appBar: AppBar(
-              title: Text('i am rhingich'),
-              backgroundColor: Colors.blueGrey,
-            ),
-            body: SafeArea(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  CircleAvatar(
-                    radius: 50,
-                    backgroundImage: AssetImage("images/bell.png"),
-                  ),
-                  Text(
-                    "hungto",
-                    style: TextStyle(
-                        fontFamily: "GreatVibes",
-                        fontSize: 40,
-                        color: Colors.red,
-                        fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    "development",
-                    style: TextStyle(
-                        fontSize: 30,
-                        color: Colors.yellow,
-                        fontFamily: "GreatVibes"),
-                  ),
-                  SizedBox(
-                    height: 20,
-                    width: 150,
-                    child: Divider(
-                      color: Colors.red,
-                    ),
-                  ),
-                  Card(
-                    color: Colors.blueGrey,
-                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-                    child: Padding(
-                      padding: EdgeInsets.all(10),
-                      child: ListTile(
-                        leading: Icon(
-                          Icons.phone,
-                        ),
-                        title: Text(
-                          "0379117280",
-                          style: TextStyle(color: Colors.red, fontSize: 20),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Card(
-                      color: Colors.red,
-                      margin:
-                          EdgeInsets.symmetric(vertical: 10, horizontal: 25),
-                      child: Padding(
-                          padding: EdgeInsets.all(10),
-                          child: ListTile(
-                            leading: Icon(Icons.mail),
-                            title: Text(
-                              "hungto228@gmail.com",
-                              style: TextStyle(
-                                  fontSize: 20,
-                                  color: Colors.black,
-                                  fontStyle: FontStyle.italic),
-                            ),
-                          ))),
-                ],
-              ),
-            )));
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: FlatButton(
+                onPressed: () {
+                  print("left button");
+                },
+                child: Image.asset("images/dice1.png")),
+          ),
+          Expanded(
+            child: FlatButton(
+                onPressed: () {
+                  print(" right bottom");
+                },
+                child: Image.asset("images/dice1.png")),
+          ),
+        ],
+      ),
+    );
   }
 }
