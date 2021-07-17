d857585a79514a266e10d59838ec3f5b6c293747
hungto228@gmail.com <hungto228@gmail.com>
2021-07-17 09:37:52 +0700

    addfunction reusableGender

--- lib/BMI/input_page.dart
+++ lib/BMI/input_page.dart
@@ -58,37 +58,33 @@ class _InputPageState extends State<InputPage> {
                 child: Row(
               children: [
                 Expanded(
-                    child: GestureDetector(
-                  onTap: () {
+                    child: ReusableCard(
+                  onpress: () {
                     setState(() {
                       selectedGender = Gender.male;
                     });
                   },
-                  child: ReusableCard(
-                    colour: selectedGender == Gender.male
-                        ? activeCardColor
-                        : inactiveCardColor,
-                    cardChild: IconContent(
-                      icon: FontAwesomeIcons.mars,
-                      lable: "Male",
-                    ),
+                  colour: selectedGender == Gender.male
+                      ? activeCardColor
+                      : inactiveCardColor,
+                  cardChild: IconContent(
+                    icon: FontAwesomeIcons.mars,
+                    lable: "Male",
                   ),
                 )),
                 Expanded(
-                    child: GestureDetector(
-                  onTap: () {
+                    child: ReusableCard(
+                  onpress: () {
                     setState(() {
                       selectedGender = Gender.female;
                     });
                   },
-                  child: ReusableCard(
-                    colour: selectedGender == Gender.female
-                        ? activeCardColor
-                        : inactiveCardColor,
-                    cardChild: IconContent(
-                      icon: FontAwesomeIcons.venus,
-                      lable: "Female",
-                    ),
+                  colour: selectedGender == Gender.female
+                      ? activeCardColor
+                      : inactiveCardColor,
+                  cardChild: IconContent(
+                    icon: FontAwesomeIcons.venus,
+                    lable: "Female",
                   ),
                 ))
               ],
--- lib/BMI/rousable_cart.dart
+++ lib/BMI/rousable_cart.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
 
 class ReusableCard extends StatelessWidget {
-  ReusableCard({required this.colour, required this.cardChild});
+  ReusableCard(
+      {required this.colour, required this.cardChild, required this.onpress});
 
   final Color colour;
   final Widget cardChild;
+  final Function()? onpress;
   @override
   Widget build(BuildContext context) {
-    return Container(
-        child: cardChild,
-        margin: EdgeInsets.all(15),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          color: colour,
-        ));
+    return GestureDetector(
+      onTap: onpress,
+      child: Container(
+          child: cardChild,
+          margin: EdgeInsets.all(15),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: colour,
+          )),
+    );
   }
 }
